d148dea5fff3f0c6b31a1a506db6a3b0355ad132
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-10-05 11:33:32 +0530

    check #4

--- meego.go
+++ meego.go
@@ -1,7 +1,11 @@
 package meego
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 var finallist string //variable to store final list of meetups
@@ -26,43 +30,23 @@ func init() {
 	fmt.Println("Working")
 }
 
-//---------
-//fetching details of meetup of the group's url
+//---------fetching details of meetup of the group's url
 func getmeetups(url string) string {
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// defer resp.Body.Close()
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// var meetups []meetuplist
-	// json.Unmarshal([]byte(body), &meetups)
-	// if (len(meetups)) > 0 {
-	// 	finallist = "Title -" + "\t" + meetups[0].Name + "\n" + "Date -" + "\t" + meetups[0].Date + "\n" + "Link -" + "\t" + meetups[0].Link + "\n\n"
-	// 	return finallist
-	// }
-	// return ""
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var meetups []meetuplist
+	json.Unmarshal([]byte(body), &meetups)
+	if (len(meetups)) > 0 {
+		finallist = "Title -" + "\t" + meetups[0].Name + "\n" + "Date -" + "\t" + meetups[0].Date + "\n" + "Link -" + "\t" + meetups[0].Link + "\n\n"
+		return finallist
+	}
+	return ""
 	return url
 }
-
-// var languages map[string]string
-
-// func init() {
-// 	languages = make(map[string]string)
-// 	languages["cs"] = "C #"
-// 	languages["js"] = "JavaScript"
-// 	languages["rb"] = "Ruby"
-// 	languages["go"] = "Golang"
-// }
-// func Get(key string) string {
-// 	return languages[key]
-// }
-// func Add(key, value string) {
-// 	languages[key] = value
-// }
-// func GetAll() map[string]string {
-// 	return languages
-// }
